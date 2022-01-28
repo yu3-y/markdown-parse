063aa9ae7e7545d9a18d3e0ecf43aef06763eb46
yu3-y <97706553+yu3-y@users.noreply.github.com>
2022-01-28 13:47:08 -0800

    1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,10 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-//import java.util.List;
+import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
    
@@ -8,11 +12,11 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-    /*/@Test
-    public void testgelinks() {
+    @Test
+    public void testgelinks() throws IOException{
+        Path filename = Path.of("test-file.md");
+        String contents = Files.readString(filename);
         List<String> op = List.of("https://something.com","some-page.html");
-    
-        assertEquals(op, MarkdownParse.getLinks("# Title [a link!](https://something.com)[another link!](some-page.html)"));
+        assertEquals(op, MarkdownParse.getLinks(contents));
     }
-    /*/
 }
