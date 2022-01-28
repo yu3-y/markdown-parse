b7e61de9d6388eb60566b7b4a036157eb0531429
yu3-y <97706553+yu3-y@users.noreply.github.com>
2022-01-27 18:21:13 -0800

    junittest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.List;
+
+public class MarkdownParseTest {
+   
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void testgelinks() {
+        List<String> op = List.of("https://something.com","some-page.html");
+    
+        assertEquals(op, MarkdownParse.getLinks("# Title [a link!](https://something.com)[another link!](some-page.html)"));
+    }
+}
