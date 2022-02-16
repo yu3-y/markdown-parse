780091c4f84f866e82ad3ec40d44870f8f7dcd3c
yu3-y <97706553+yu3-y@users.noreply.github.com>
2022-02-16 11:07:59 -0800

    j

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -38,7 +38,7 @@ public class MarkdownParse {
         }
             return toReturn;
     }
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
